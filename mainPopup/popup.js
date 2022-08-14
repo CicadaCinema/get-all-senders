3f50e129c05f5bacdc43d67c51ac1e611c84ccc4
CicadaCinema <52425971+CicadaCinema@users.noreply.github.com>
2022-08-14 14:22:56 +0100

    add page counter

--- mainPopup/popup.js
+++ mainPopup/popup.js
@@ -1,13 +1,20 @@
+function setInfoText(text) {
+	document.getElementById("popup-page").innerHTML = text;
+}
+
 async function load() {
 	const senders = new Set();
 
 	let page = await messenger.messages.query({});
+	let counter = 0;
 
+	setInfoText(`Processing page ${counter++}...`);
 	for (let message of page.messages) {
 		senders.add(message.author)
 	}
 
 	while (page.id) {
+		setInfoText(`Processing page ${counter++}...`);
 		page = await messenger.messages.continueList(page.id);
 		for (let message of page.messages) {
 			senders.add(message.author)
@@ -20,7 +27,7 @@ async function load() {
 	})
 
 	navigator.clipboard.writeText(text);
-	document.getElementById("popup-page").innerHTML = "Done! Check your clipboard.";
+	setInfoText("Done! Check your clipboard.");
 }
 
 document.addEventListener("DOMContentLoaded", load);
